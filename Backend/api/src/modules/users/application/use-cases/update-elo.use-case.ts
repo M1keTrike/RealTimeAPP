c3bdf4b42487b49cbec5cac074911ce4367566f5
Miguel Ángel MOlina Gómez <miguemolina4570@gmail.com>
2026-02-26 21:48:05 -0600

    [FIX] patch elo endpoint

---
+++ Backend/api/src/modules/users/application/use-cases/update-elo.use-case.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import type { IUserRepository } from '../../domain/repositories/user.repository.interface';
+import { USER_REPOSITORY } from '../../domain/repositories/user.repository.interface';
+import { User } from '../../domain/entities/user.entity';
+
+@Injectable()
+export class UpdateEloUseCase {
+  constructor(
+    @Inject(USER_REPOSITORY)
+    private readonly userRepo: IUserRepository,
+  ) {}
+
+  async execute(userId: string, newEloRating: number): Promise<User> {
+    const user = await this.userRepo.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`Usuario con ID ${userId} no encontrado.`);
+    }
+    await this.userRepo.updateEloRating(userId, newEloRating);
+    user.eloRating = newEloRating;
+    return user;
+  }
+}
